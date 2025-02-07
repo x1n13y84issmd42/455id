521ae56d7addb53a506d1958179c95d54469c6f1
x1n13y84issmd42 <x1n13y84issmd42@gmail.com>
2025-02-07 12:38:19 +0200

    Renamed services to modules.

--- src/exec/IDApp.module.ts
+++ src/exec/IDApp.module.ts
@@ -1,6 +1,6 @@
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { TestController } from "../controllers/Test.controller";
-import { AuthModule } from "../services/Auth";
+import { AuthModule } from "../modules/Auth";
 import { ExceptionFiltersModule } from "@x1n13y84issmd42/455fw";
 
 @Module({
